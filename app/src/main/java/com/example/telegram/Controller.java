c70be512ff70b9218660e744b03f5515d2efe367
tabatabaeian <artabatabaeian@ce.sharif.edu>
2019-02-26 18:15:26 +0330

    First

---
+++ app/src/main/java/com/example/telegram/Controller.java
@@ -0,0 +1,20 @@
+package com.example.telegram;
+
+public class Controller {
+    private static final Controller ourInstance = new Controller();
+
+    public static Controller getInstance() {
+        return ourInstance;
+    }
+
+    private Controller() {
+    }
+
+    public void onComplite(){
+
+    }
+
+    public void doJob(){
+
+    }
+}
---
+++ app/src/main/java/com/example/telegram/NotifCenterCallback.java
@@ -0,0 +1,7 @@
+package com.example.telegram;
+
+public interface NotifCenterCallback {
+
+    public void run(int id, Object res);
+
+}
