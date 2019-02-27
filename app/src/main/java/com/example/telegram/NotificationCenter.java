915d622c748004ee1b47030c74cb2a5f4a5b68d1
DarkMarshal77 <tabanavid77@gmail.com>
2019-02-27 16:27:16 +0330

    NotificationCenter Demo

--- app/src/main/java/com/example/telegram/NotificationCenter.java
+++ app/src/main/java/com/example/telegram/NotificationCenter.java
@@ -1,39 +1,56 @@
 package com.example.telegram;
 
+import android.support.annotation.UiThread;
 import android.util.SparseArray;
 
 import java.util.ArrayList;
 
 public class NotificationCenter {
-    private NotificationCenter notificationCenter;
+    private static final int DATA_LOADED = 0x47;
+
+    public interface NotificationCenterDelegate {
+        void didReceivedNotification(int id, Object... args);
+    }
+
+    private NotificationCenter notificationCenter = new NotificationCenter();
     private SparseArray<ArrayList<Object>> observers = new SparseArray<>();
 
     private NotificationCenter(){
 
     }
 
+    @UiThread
     public NotificationCenter getInstance(){
-        if (notificationCenter == null)
-            notificationCenter = new NotificationCenter();
-
         return notificationCenter;
     }
 
-    public void add(NotifCenterCallback notifCenterCallback, int id){
+    public void addObserver(Object observer, int id){
         ArrayList<Object> objects = observers.get(id);
-        if (objects == null)
-            observers.put(id, new ArrayList<Object>());
-        if (objects.contains(notifCenterCallback))
+        if (objects == null){
+            objects = new ArrayList<Object>();
+            observers.put(id, objects);
+        }
+
+        if (objects.contains(observer))
             return;
 
-        objects.add(notifCenterCallback);
+        objects.add(observer);
     }
 
-    public void post(int id, Object result){
+    public void removeObserver(Object observer){
+        for (int i = 0 ; i < observers.size() ; i++){
+            ArrayList<Object> objects = observers.get(i);
+            if (objects != null)
+                objects.remove(observer);
+        }
+    }
+
+    @UiThread
+    public void post(int id, Object... args){
         ArrayList<Object> objects = observers.get(id);
 
         for (int i = 0; i < objects.size() ; i++) {
-            ((NotifCenterCallback)objects.get(i)).run(id, result);
+            ((NotificationCenterDelegate)objects.get(i)).didReceivedNotification(id, args);
         }
     }
 }
