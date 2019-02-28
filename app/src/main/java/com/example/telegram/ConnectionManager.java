1138d9e4d84c1b0131f9f14def8cb9fa90173e6d
sana <ayromlous@gmail.com>
2019-02-28 12:11:39 +0330

    Connection Manager

--- app/src/main/java/com/example/telegram/ConnectionManager.java
+++ app/src/main/java/com/example/telegram/ConnectionManager.java
@@ -1,4 +1,25 @@
 package com.example.telegram;
 
+import java.util.ArrayList;
+
 public class ConnectionManager {
+    ArrayList<Integer> loadR;
+    private static final ConnectionManager CONNECTION_MANAGER = new ConnectionManager();
+    public static ConnectionManager getInstance(){return CONNECTION_MANAGER;}
+    private ConnectionManager(){
+
+    }
+    public ArrayList<Integer> load(Integer param){
+        try {
+            this.wait(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        int i = 1;
+        while (i<=10){
+            loadR.add(i+param);
+            i++;
+        }
+        return loadR;
+    }
 }
